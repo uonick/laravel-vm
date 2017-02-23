98597087bb6416774b2187f457b837c27e112de3
uonick <uonick@users.noreply.github.com>
2017-02-23 11:56:10 +0300

    + nodejs

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -2,3 +2,6 @@ echo -e "\n--- COMPOSER ---\n"
 sudo curl -sS https://getcomposer.org/installer | php > /dev/null
 sudo mv composer.phar /usr/local/bin/composer
 sudo apt install npm
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
