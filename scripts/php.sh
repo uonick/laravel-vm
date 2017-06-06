e5a27cdd92da3c9db021ed4050981ee83afde5a0
uonick <uonick@users.noreply.github.com>
2017-06-06 09:43:59 +0300

    + opcache

--- scripts/php.sh
+++ scripts/php.sh
@@ -1,2 +1,2 @@
 echo -e "\n--- PHP ---\n"
-sudo apt-get install -y php7.1 php7.1-cli php7.1-common php7.1-mysql php7.1-fpm php7.1-curl php7.1-gd php-imagick php7.1-mcrypt php7.1-json php7.1-tidy php7.1-mbstring php-memcached php7.1-zip php7.1-xml mcrypt
+sudo apt-get install -y php7.1 php7.1-cli php7.1-common php7.1-mysql php7.1-fpm php7.1-curl php7.1-gd php-imagick php7.1-mcrypt php7.1-json php7.1-tidy php7.1-mbstring php-memcached php7.1-zip php7.1-xml mcrypt php7.1-opcache
