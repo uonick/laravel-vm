3835fa475e14a24b2359a7fd70cf9f036240a0aa
uonick <uonick@users.noreply.github.com>
2017-11-24 12:48:37 +0500

    Update mysql.sh

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -8,6 +8,6 @@ sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again p
 sudo apt-get install -y mysql-server mysql-client
 sudo sed -i 's/bind-address/bind-address = 0.0.0.0#/' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO '$DBUSER'@'%' IDENTIFIED BY '$DBUSER' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-sudo echo 'character-set-server=utf8' >> /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo echo 'character-set-server=utf8mb4' >> /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo echo 'collation-server=utf8mb4_unicode_ci' >> /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo service mysql restart
