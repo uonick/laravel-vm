46d96fad040391e35456263c6f8c1698abbcef5f
uonick <uonick@users.noreply.github.com>
2017-11-24 12:20:57 +0500

    laravel mb4 update

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -9,5 +9,5 @@ sudo apt-get install -y mysql-server mysql-client
 sudo sed -i 's/bind-address/bind-address = 0.0.0.0#/' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO '$DBUSER'@'%' IDENTIFIED BY '$DBUSER' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 sudo echo 'character-set-server=utf8' >> /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo echo 'collation-server=utf8_general_ci' >> /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo echo 'collation-server=utf8mb4_unicode_ci' >> /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo service mysql restart
