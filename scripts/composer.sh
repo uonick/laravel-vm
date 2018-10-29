7190a15ea0f0bfe4c5e5d4b72823efd44b8a9191
uonick <365029+uonick@users.noreply.github.com>
2018-10-29 13:31:48 +0500

    phpcs added

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -1,3 +1,5 @@
 echo -e "\n--- COMPOSER ---\n"
 sudo curl -sS https://getcomposer.org/installer | php > /dev/null
 sudo mv composer.phar /usr/local/bin/composer
+composer global require "squizlabs/php_codesniffer=*"
+sudo ln -s /home/vagrant/.config/composer/vendor/bin/phpcs /usr/local/bin/phpcs
